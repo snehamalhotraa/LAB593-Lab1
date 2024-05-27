bfaabe9bc6718da23bcf8808aa10ee86d6359a38
snehamalhotraa <sneha3@flemingcollege.ca>
2024-05-27 13:37:39 -0400

    Modified both files in the repo clone
    -Deleted a print() statement from script1.py.
    -Added one print() statement to script2.py.

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 #Print lines from Zen of Python
 print("Now is better than never.")
-print("Although never is often better than *right* now.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 #Print lines from Zen of Python
 print("Flat is better than nested.")
 print("Sparse is better than dense.")
+print("Namespaces are one honking great idea--let's do more of those!")
